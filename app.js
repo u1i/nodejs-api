e8bf28366b1aeb293e8e7ca65eef888e7771e6c8
Uli Hitzel <uli.hitzel@gmail.com>
2021-04-14 08:48:19 +0800

    changed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.listen(8080, () => {
 });
 
 app.get("/", (req, res) => {
- res.send(["Hello! This is version 0.6"]);
+ res.send(["Hello! This is version 0.7"]);
 });
 
 app.get("/api", (req, res, next) => {
