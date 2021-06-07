e947aee1a302636cefbad0d3226b74726a576aca
Uli <uli.hitzel@gmail.com>
2021-06-07 15:48:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.listen(8080, () => {
 });
 
 app.get("/", (req, res) => {
- res.send(["Hello! This is version 0.9"]);
+ res.send(["Hello! This is version 0.10"]);
 });
 
 app.get("/api", (req, res, next) => {
@@ -25,7 +25,7 @@ app.get("/fx", (req, res, next) => {
  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
  var dateTime = date+' '+time;
 
- res.json({"sell": "489." + rand1, "timestamp": dateTime, "buy": "389." + rand2});
+ res.json({"sell": "487." + rand1, "timestamp": dateTime, "buy": "387." + rand2});
 });
 
 app.get("/random", (req, res, next) => {
