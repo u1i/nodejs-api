325ef7f6205b237be8e6908b5e90ef960e5889d3
Uli <uli.hitzel@gmail.com>
2022-11-22 15:55:03 +0800

    changed version number to 0.23

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.listen(8080, () => {
 });
 
 app.get("/", (req, res) => {
- res.send(["Hello! This is version 0.22"]);
+ res.send(["Hello! This is version 0.23"]);
 });
 
 app.get("/api", (req, res, next) => {
